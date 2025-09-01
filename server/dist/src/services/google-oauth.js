cdd28ff36e513011bd9163aa5b169304cf938fe2
Rohit <rohit.rthd.04@gmail.com>
2025-09-02 02:05:26 +0530

    changes in oauth callback url config

--- server/dist/src/services/google-oauth.js
+++ server/dist/src/services/google-oauth.js
@@ -5,7 +5,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: "http://localhost:8000/api/googleauth/google/callback",
-    callbackURL: "/api/googleauth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, async (_accessToken, _refreshToken, profile, done) => {
     try {
         // let user = await User.findOne({
--- server/src/services/google-oauth.ts
+++ server/src/services/google-oauth.ts
@@ -8,7 +8,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       // callbackURL: "http://localhost:8000/api/googleauth/google/callback",
-      callbackURL: "/api/googleauth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
