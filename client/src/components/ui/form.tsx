66f0bd009df131d959b66cafe339b62caf308974
Rohit <rohit.rthd.04@gmail.com>
2025-09-03 05:34:38 +0530

    shadcn components import alias errors and un-used vars errors config done in tsconfig.json

--- client/src/components/ui/form.tsx
+++ client/src/components/ui/form.tsx
@@ -11,8 +11,8 @@ import {
   type FieldValues,
 } from "react-hook-form"
 
-import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
+import { cn } from "../../lib/utils"
+import { Label } from "../../components/ui/label"
 
 const Form = FormProvider
 
@@ -154,7 +154,7 @@ function FormMessage({ className, ...props }: React.ComponentProps<"p">) {
 }
 
 export {
-  useFormField,
+  // useFormField,
   Form,
   FormItem,
   FormLabel,
